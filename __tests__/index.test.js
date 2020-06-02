c56db8c14cd4c6e2c593f85d26e0f45b788119b9
bekkun <bekkunilme@tutye.com>
2020-06-02 23:23:13 +0400

    refactor tests refactor parsers

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,51 +4,20 @@ import gendiff from '../src';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const pathToJsonFile1 = getFixturePath('before.json');
-const pathToJsonFile2 = getFixturePath('after.json');
-const pathToYmlFile1 = getFixturePath('before.yml');
-const pathToYmlFile2 = getFixturePath('after.yml');
-const pathToIniFile1 = getFixturePath('before.ini');
-const pathToIniFile2 = getFixturePath('after.ini');
-
-const pathToJsonRecursFile1 = getFixturePath('before_r.json');
-const pathToJsonRecursFile2 = getFixturePath('after_r.json');
-
-const pathToCorrectPretty = getFixturePath('correctPrettyFormat.txt');
-const pathToCorrectJsonRecursive = getFixturePath('correctTreeFormat.txt');
-const pathToCorrectPlain = getFixturePath('correctPlainOutput.txt');
-const pathToCorrectJson = getFixturePath('correctJsonFormat.txt');
-
-const correctPlainFormat = readFileSync(pathToCorrectPlain, 'utf8');
-const correctTreeFormat = readFileSync(pathToCorrectJsonRecursive, 'utf8');
-const correctPrettyFormat = readFileSync(pathToCorrectPretty, 'utf8');
-const correctJsonFormat = readFileSync(pathToCorrectJson, 'utf8');
+const correctPlainFormat = readFileSync(getFixturePath('correctPlainOutput.txt'), 'utf8');
+const correctTreeFormat = readFileSync(getFixturePath('correctTreeFormat.txt'), 'utf8').trim();
+const correctJsonFormat = readFileSync(getFixturePath('correctJsonFormat.txt'), 'utf8').trim();
 
 describe('compare two files', () => {
+  const pathToJsonRecursFile2 = getFixturePath('after_r.json');
+  const pathToJsonRecursFile1 = getFixturePath('before_r.json');
+
   test.each([
-    [pathToJsonFile1, pathToJsonFile2],
-    [pathToYmlFile1, pathToYmlFile2],
-    [pathToIniFile1, pathToIniFile2],
-  ])('in Pretty format (json, yml, ini)', (a, b) => {
-    const result = gendiff(a, b, 'tree');
-    expect(result).toEqual(correctPrettyFormat.trim());
-  });
-  test.each([
-    [pathToJsonRecursFile1, pathToJsonRecursFile2],
-  ])('recursivly', (a, b) => {
-    const result = gendiff(a, b, 'tree');
-    expect(result).toEqual(correctTreeFormat.trim());
-  });
-  test.each([
-    [pathToJsonRecursFile1, pathToJsonRecursFile2],
-  ])('in plain format', (a, b) => {
-    const result = gendiff(a, b, 'plain');
-    expect(result).toEqual(correctPlainFormat);
-  });
-  test.each([
-    [pathToJsonRecursFile1, pathToJsonRecursFile2],
-  ])('in json format', (a, b) => {
-    const result = gendiff(a, b, 'json');
-    expect(result).toEqual(correctJsonFormat.trim());
+    [pathToJsonRecursFile1, pathToJsonRecursFile2, correctTreeFormat, 'tree'],
+    [pathToJsonRecursFile1, pathToJsonRecursFile2, correctPlainFormat, 'plain'],
+    [pathToJsonRecursFile1, pathToJsonRecursFile2, correctJsonFormat, 'json'],
+  ])('recursivly', (a, b, expected, format) => {
+    const result = gendiff(a, b, format);
+    expect(result).toEqual(expected);
   });
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,23 +1,7 @@
 import { safeLoad } from 'js-yaml';
-import { resolve } from 'path';
-import { readFileSync } from 'fs';
+
 import { parse } from 'ini';
 
-export const parseYml = (filename) => {
-  const pathToFile = resolve(process.cwd(), filename);
-  const content = readFileSync(pathToFile, 'utf8');
-
-  return safeLoad(content);
-};
-
-export const parseJson = (filename) => {
-  const pathToFile = resolve(process.cwd(), filename);
-  const content = readFileSync(pathToFile, 'utf8');
-  return JSON.parse(content);
-};
-
-export const parseIni = (filename) => {
-  const pathToFile = resolve(process.cwd(), filename);
-  const content = readFileSync(pathToFile, 'utf8');
-  return parse(content);
-};
+export const parseYml = (content) => safeLoad(content);
+export const parseJson = (content) => JSON.parse(content);
+export const parseIni = (content) => parse(content);
