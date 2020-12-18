b45d028eb153c1db33b0867b6266e481a736d13c
bekkun <bekkunilme@tutye.com>
2020-12-18 16:06:16 +0400

    refactor stylish format

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,46 +3,47 @@ import _ from 'lodash';
 export default (ast) => {
   const doIndent = (depth) => '  '.repeat(depth);
 
-  const stringify = (node, depth = 0) => {
+  const stringify = (node, depth) => {
     if (!_.isObject(node)) {
-      return ` ${node}`;
+      return `${node}`;
     }
-
     const pairs = _.toPairs(node);
     const newNode = pairs.map((pair) => {
       const [key, value] = pair;
       if (!_.isObject(value)) {
-        return `${doIndent(depth + 2)}  ${key}: ${value}\n`;
+        return `      ${doIndent(depth)}  ${key}: ${value}`;
       }
-      return `${doIndent(depth + 2)}  ${key}:${stringify(value, depth + 2)}\n`;
+      return `      ${doIndent(depth)}  ${key}: ${stringify(value, depth + 2)}`;
     });
 
-    return ` {\n${newNode.join('')}${doIndent(depth + 1)}}`;
+    return `{\n${newNode.join('\n')}\n    ${doIndent(depth)}}`;
   };
 
   const iter = (tree, depth) => {
     const nodes = tree.map((node) => {
       const {
-        key, value, status, after, before, children,
+        key, value, status, after, before, children, type,
       } = node;
 
-      if (node.type === 'hasNoNested') {
+      if (type === 'hasNoNested') {
         switch (status) {
           case 'updated':
-            return `${doIndent(depth)}- ${key}:${stringify(before, depth)}\n${doIndent(depth)}+ ${key}:${stringify(after, depth)}\n`;
+            return `  ${doIndent(depth)}- ${key}: ${stringify(before, depth)}\n  ${doIndent(depth)}+ ${key}: ${stringify(after, depth)}`;
           case 'added':
-            return `${doIndent(depth)}+ ${key}:${stringify(value, depth)}\n`;
+            return `  ${doIndent(depth)}+ ${key}: ${stringify(value, depth)}`;
           case 'removed':
-            return `${doIndent(depth)}- ${key}:${stringify(value, depth)}\n`;
+            return `  ${doIndent(depth)}- ${key}: ${stringify(value, depth)}`;
           default:
-            return `${doIndent(depth)}  ${key}:${stringify(value, depth)}\n`;
+            return `  ${doIndent(depth)}  ${key}: ${stringify(value, depth)}`;
         }
       }
-      return `${doIndent(depth)}  ${key}: {\n${iter(children, depth + 2)}${doIndent(depth + 1)}}\n`;
+      const newDepth = depth + 2;
+
+      return `    ${doIndent(depth)}${key}: {\n${iter(children, newDepth)}\n    ${doIndent(depth)}}`;
     });
 
-    return nodes.join('');
+    return nodes.join('\n');
   };
 
-  return `{\n${iter(ast, 1)}}\n`;
+  return `{\n${iter(ast, 0)}\n}\n`;
 };
